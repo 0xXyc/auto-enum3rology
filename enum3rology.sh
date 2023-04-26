01f716472d3ea45491e42f3fc59c753879b8477e
alexrf45 <fonalex45@gmail.com>
2023-04-25 21:59:24 -0400

    Script overhaul, recommendations on current code in enum3rology_w_comments.sh

--- enum3rology.sh
+++ enum3rology.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# This script is a massive work-in-progress and will be changed many, many times.
-# The motivation behind this script is to mash together MY entire enumeration methodology in an effort to save as much time as possible during engagements or in an exam environment.
-# Still deciding on a name... Enum3r8 or Enumerology (enumeration+methodology)
+# I took out dirsearch, feroxbuster, and gobuster and went with ffuf to simplify things. I find ffuf can pretty much do it all with
+# less overhead. Additionally, I removed nikto as in my experience I don't really glean anything useful from it on CTFs and it takes way too
+# long to run with minimal return on investment.
+#I tend to stick to rustscan, ffuf, and whatweb for the majority of my initial linux recon
+# I vote Enum3r8 for the tool name.
 
-set -e 
+set -e
 
 Red='\033[0;31m'
 # Red
@@ -15,9 +17,10 @@ Blue='\033[0;34m'
 Color_Off='\033[0m'
 # No color
 
+#I decided to remove sudo
 if ((EUID != 0)); then
-  echo >&2 "Error: script not running as root or with sudo! Exiting..."
-  exit 1
+	echo >&2 "Error: script not running as root or with sudo! Exiting..."
+	exit 1
 fi
 
 echo -e "${Blue}
@@ -33,129 +36,150 @@ echo -e "${Blue}
                                                                               ░ ░     
 "
 
-echo -e "${Green}                       Created by: https://github.com/jswinss\n"
-echo -e "${Blue}Current Version: 0.3\n"
+echo -e "${Green}Created by: https://github.com/jswinss\n"
+echo -e "${Red}Edited script by https://github/alexrf45"
 sleep 2
 
 # Placing logic here for command line arg for obtaining IP address to be enumerated through the duration of the script
 echo -e "${Red}Disclaimer: Enum3rology is not to be used as a full proof enumeration tool. You should still rely on your manual methodology!\n${Color_Off}"
 sleep 1
 
-# Check that an IP address was provided as an argument
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <target_ip_address> <target_url> <hostname>"
-    exit 1
+# Check that an IP address and target name are provided as arguments
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <target_ip_address> <target_name>"
+	exit 1
 fi
 
-# Store the IP address in a variable
+# Store the IP address in a variable and set User-Agent as desired
 ip_address=$1
-target_url=$2
-hostname=$3
+target_name=$2
 
+#dependency checks are awesome!!
 echo -e "${Blue}Performing dependency checks:${Color_Off}"
 sleep 1
-echo -e "${Green}Checking if nmap is installed..."
-if ! which nmap > /dev/null; then
-   echo -e "Command not found! Install? (y/n) \c"
-   read
-   if [ "$REPLY" = "y" ]; 
-   then
-      echo -e ''
-      sudo apt-get install nmap
-   fi
+echo -e "${Green}Checking if nmap, ffuf & whatweb are installed..."
+if ! which nmap && which ffuf && which whatweb >/dev/null; then
+	echo -e "tools not found! Install? (y/n) \c"
+	read
+	if [ "$REPLY" = "y" ]; then
+		echo -e ''
+		sudo apt-get install nmap ffuf whatweb -y -qq >/dev/null
+	fi
 else
-    echo -e "Already installed!"
-    echo -e ''
-    sleep 1
-fi
-
-echo -e "${Green}Checking if dirsearch is installed..."
-if ! which dirsearch > /dev/null; then
-   echo -e "Command not found! Install? (y/n) \c"
-   read
-   if [ "$REPLY" = "y" ]; 
-   then
-      echo -e ''
-      sudo apt-get install dirsearch
-   fi
-else
-    echo -e "Already installed!"
-    echo -e ''
-    sleep 1
-fi
-
-echo -e "${Green}Checking if feroxbuster is installed..."
-if ! which feroxbuster > /dev/null; then
-   echo -e "Command not found! Install? (y/n) \c"
-   read
-   if [ "$REPLY" = "y" ]; 
-   then
-      echo -e ''
-      sudo apt-get install feroxbuster
-   fi
-else
-    echo -e "Already installed!"
-    echo -e ''
-    sleep 1
+	echo -e "Already installed!"
+	echo -e ''
+	sleep 1
 fi
 
 # Print the IP address to the console
 echo -e "${Green}Targets:\n         IP: $ip_address"
-echo -e "${Green}         Target URL: $target_url"
-echo -e "${Green}         Target Hostname: $hostname\n"
+echo -e "${Green}         Target Name: $target_name"
 read -p "Press ENTER to begin enumerating your target!"
 echo -e "${Color_Off}"
 
 # Performs a series nmap scans to the target IP address
-rm -rf Enum3rology_Output
-mkdir Enum3rology_Output
-cd Enum3rology_Output
-echo -e "${Blue}Beginning Nmap scans..."
-echo -e "Now performing a TCP scan of all 65,535 ports on the target (-sV, -sC. -v):${Color_Off}"
-nmap -sV -sC -v $ip_address -oN TCP-Scan
-echo -e "${Blue}\nDo you want to conduct a UDP scan on your target?"
-echo -e "(y/n)"
-read
-if [ "$REPLY" = "y" ];
-    then
-      echo ''
-      echo -e "${Blue}Now performing a UDP scan of the top 1,000 ports on the target (-sU -sV -sC -v) "
-      echo ''
-      nmap -sU -sV -sC -v $ip_address -oN UDP-Scan
-
+if [ -d "./Enum3rology_Output" ]; then
+	rm -rf Enum3rology_Output
 else
-    echo -e "Skipping UDP scan, let's continue enumerating!"
+	mkdir Enum3rology_Output && cd Enum3rology_Output
 fi
 
-echo -e "\n${Blue}The All scan output will be saved to TCP-scan"
-echo -e "The UDP scan output will be saved to UDP-scan${Color_Off}"
+function nmap_scan() {
+
+	echo -e "${Blue}Beginning Nmap scans..."
+	echo -e "Now performing a TCP scan of all 65,535 ports on the target (-sV, -sC. -v):${Color_Off}"
+	nmap -sV -sC -v $ip_address -oN $target_name-TCP-Scan
+	echo -e "${Blue}\nDo you want to conduct a UDP scan on your target?\n${Color_Off}"
+	echo -e "(y/n)"
+	read
+	if [ "$REPLY" = "y" ]; then
+		echo ''
+		echo -e "${Blue}Now performing a fast UDP scan. re-run manually to confirm results,"
+		echo -e "${Blue}as this scan is very fast and sends alot of packets\n adjust --min-rate for unstable targets${Color_Off}"
+		sudo nmap -sU --min-rate 10000 -oN $target_name-UDP-Scan $ip_address -v
+	else
+		echo -e "Skipping UDP scan, let's continue enumerating!"
+	fi
+
+	echo -e "\n${Blue}The All scan output will be saved to TCP-scan"
+	echo -e "The UDP scan output will be saved to UDP-scan${Color_Off}"
+}
 
 # Performs a directory bruteforce on the specified target URL
-echo -e "\n${Blue}Beginning directory bruteforce scan on $target_url:${Color_Off}"
-dirsearch -u $target_url
-echo -e "${Blue}\nBeginning recursive directory bruteforce with Feroxbuster${Color_Off}"
-feroxbuster -u $target_url -e -g -o feroxbuster-report.txt
+# I do rate limiting to be more kind to the box. It's more realistic and good to practice.
+# I also set a custom user-agent to practice for bug bounty
 
-# Performs subdomain enumeration of the target_url -- This one requires more testing due to lots of output and a need to re-run with a filter... perhaps put a note at the end of the script saying to do this?
-echo -e "\n${Blue}Beginning subdomain enumeration:${Color_Off}"
-gobuster dns -d $hostname -w /usr/share/wordlists/dirb/common.txt
+function ffuf_directory() {
+	echo -e "\n${Blue}Beginning directory bruteforce scan on $target_name:${Color_Off}"
+
+	ffuf -c -t 50 -p 0.1 -rate 100 \
+		-H "User-Agent: $AGENT" -ac \
+		-o $target_name.csv -of csv \
+		-mc 200,302,403 \
+		-w /usr/share/seclists/Discovery/Web-Content/raft-small-words.txt \
+		-u http://$DOMAIN/FUZZ
+
+}
+
+# Performs subdomain enumeration of the target_url using ffuf
+
+# I do rate limiting to be more kind to the box. It's more realistic and good to practice.
+
+function ffuf_subdomain() {
+	echo -e "\n${Blue}Beginning subdomain enumeration:${Color_Off}"
+
+	ffuf -c -t 40 -p 0.1 -rate 100 \
+		-o $DOMAIN.csv -of csv \
+		-H "User-Agent: $AGENT" -ac \
+		-H "Host: FUZZ.$DOMAIN" \
+		-w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt \
+		-u http://$DOMAIN/ \
+		-mc 200,403
+}
 
 # Performs web server fingerprinting with whatweb
-echo -e "\n${Blue}Fingerprinting web server with whatweb.${Color_Off}"
-whatweb $target_url -a 3 -v
 
-# Performs a vulnerability scan with Nikto
-echo -e "\n${Blue}Starting vulnerability scan with Nikto. This may take awhile...${Color_Off}"
-nikto -h $ip_address -output nikto-output.txt
+#Here's a one liner I use for whatweb for CTFs and Bug Bounty. You can also set a custom user agent
+#for tools so WAFs and other tooling are less likely to detect it.
+
+function whatweb_scan() {
+	whatweb -a 1 -t 10 -U="$AGENT" -t 10 --wait=0.2 http://$ip_address/ --log-brief=output.txt
+}
+
+#I also added some commands to extract the domain name from whatweb for use in ffuf.
+#unfurl must be installed for this to work
+
+#Ref: https://github.com/tomnomnom/unfurl
+
+#quick installation instructions for testing
+#wget https://github.com/tomnomnom/unfurl/releases/download/v0.0.1/unfurl-linux-amd64-0.0.1.tgz
+#tar xzf unfurl-linux-amd64-0.0.1.tgz
+#sudo mv unfurl /usr/bin/
+
+function domain_extract() {
+	grep -n ".htb".. output.txt |
+		cut -d ' ' -f 10 | cut -d "[" -f 2 | cut -d "]" -f 1 >url.txt &&
+		cat url.txt | unfurl domains >domain.txt
+
+}
+
+#exports the domain name to variable for use in ffuf
+function domain_add() {
+	sudo echo "${ip_address} $(cat domain.txt)" >>/etc/hosts && export DOMAIN=$(cat domain.txt)
+}
+#web enumeration function execution, I prefer to build functions and then call them later in the script
+#This way if I need to remove one I can just remove the desired function call below
+
+echo -e "\n${Blue}Fingerprinting web server with whatweb.${Color_Off}"
+whatweb_scan
+domain_extract
+domain_add
+nmap_scan
+ffuf_directory
+ffuf_subdomain
 
 # -------------------------------------------------------------------------------------------------------------------------------
-# Additional Tips Section -- Still need to implement
+# Useful Tips Section:
 echo -e "\n${Green}Things that I can't do that you should:"
 echo -e "View the source code of the web site of HTML and blank PHP pages for secrets"
 echo -e "Visually inspect the web server and manipulate the web app to see if you can get unexpected behavior ;)${Color_Off}"
-echo -e "${Blue}Additional commands that should be ran manually:${Color_Off}"
-echo -e "ffuf -u $target_url -H "Host: FUZZ.$hostname" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt"
-
-# To-Do List:
-# WPScan option
-#--------------------------------------------------------------------------------------------------------------------------------
